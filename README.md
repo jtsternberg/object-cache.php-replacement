4fc7a25b54319b22fd1279411b502c5e0f675c3c
Justin Sternberg <justin@dsgnwrks.pro>
2018-02-14 12:04:55 -0500

    Add the object-cache.php file and update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# object-cache.php-replacement
-Loading the Optimal WordPress Object Cache Implementation
+# `object-cache.php` Replacement
+
+Handles checking for persistent object cache (in this case, Redis and uses the [WP Redis](https://wordpress.org/plugins/wp-redis/)), and if it is found, loads it. If not, falls back (in a bit of a hacky way) to the WordPress default cache system.
+
+Blog post found here: [Loading the Optimal WordPress Object Cache Implementation](https://webdevstudios.com/2015/11/03/loading-the-optimal-wordpress-object-cache-implementation-in-your-production-staging-and-local-development-environments/)
---
+++ object-cache.php
@@ -0,0 +1,49 @@
+<?php
+
+// If Redis exists and redis server is configured,
+if ( class_exists( 'Redis' ) && isset( $GLOBALS['redis_server'] ) ) {
+
+	// Then load our object cache plugin.
+	require_once 'plugins/wp-redis/object-cache.php';
+}
+
+/*
+ * If we can't use the object-cache, we need some trickery
+ * for WP to believe we aren't actually using an object-cache
+ * (which it assumes since we have this file)
+ */
+else {
+
+	// Helper/callback.
+	function set_wp_using_ext_object_cache_to_false() {
+		wp_using_ext_object_cache( false );
+	}
+
+	/*
+	 * Set to false now.
+	 * (After this file loads, WP resets to true.)
+	 */
+	set_wp_using_ext_object_cache_to_false();
+
+	/*
+	 * Loads and caches certain often requested site options. Need
+	 * to do manually now because it will not run later when
+	 * wp_load_core_site_options is set to false.
+	 */
+	if ( is_multisite() ) {
+		add_action( 'ms_loaded', 'set_wp_using_ext_object_cache_to_false', 0 );
+		add_action( 'ms_loaded', 'wp_load_core_site_options', 2 );
+	} else {
+		wp_load_core_site_options();
+	}
+
+	// Include the built-in WP object-caching.
+	require_once( ABSPATH . WPINC . '/cache.php' );
+
+	// Hook in to reset to false,
+	add_action(
+		'muplugins_loaded', // To the earliest hook,
+		'set_wp_using_ext_object_cache_to_false',
+		-9999 // At a super early priority.
+	);
+}
